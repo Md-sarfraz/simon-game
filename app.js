335747084ecbcee06048b2c21db2008e5876279d
Md-sarfraz <mdsarfraz62070@gmail.com>
2024-02-27 11:27:26 +0530

    update level up part

--- app.js
+++ app.js
@@ -25,17 +25,37 @@ function btnFlash(btn){
 }
  
  function levelUp(){
+    userSeq=[];
     level++;
     h2.innerText=`level ${level}`;
     let randIdx = Math.floor(Math.random() * 4);
     let randColor=btns[randIdx];
     let randBtn=document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor)
+    console.log(gameSeq)
     btnFlash (randBtn);
+}
+function checkAns(idx){
+    // console.log("current level :",level)
+    // let idx=level-1;
+    if(userSeq[idx]==gameSeq[idx])
+    {
+        if(userSeq.length==gameSeq.length){
+           setTimeout(levelUp,1000)
+        }
+    }
+    else{
+        h2.innerText="Game Over! press any keys to start"
+    }
 }
 
  function btnPress(){
     let btn=this;
     btnFlash(btn);
+    userColor=btn.getAttribute("id")
+    userSeq.push(userColor)
+    console.log(userSeq)
+    checkAns(userSeq.length-1);
 }
 allBtns=document.querySelectorAll(".btn");
 for(btn of allBtns){
